3dc44b31a37aa7cbba9cab1d775a4451e2b59767
galileoguzman <galileoguzman@gmail.com>
2023-12-13 08:53:54 -0600

    Initial Commit

---
+++ Coordinator-SwiftUI/Coordinator_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coordinator_SwiftUIApp.swift
+//  Coordinator-SwiftUI
+//
+//  Created by Galileo Guzman on 13/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Coordinator_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
